37d88d74cc136c68b1e5062cb5d4eca8c3912116
yuan-0816 <wj582693@gmail.com>
2023-10-12 22:47:26 +0800

    add nav2 launch file

--- AirLab_in_gazebo/launch/nav2.launch.py
+++ AirLab_in_gazebo/launch/nav2.launch.py
@@ -1,16 +1,8 @@
-# Copyright (c) 2018 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
+
+
+# ros2 launch AirLab_in_gazebo nav2.launch.py headless:=False
+
 
 """This is all-in-one launch script intended for use by nav2 developers."""
 
@@ -140,10 +132,6 @@ def generate_launch_description():
 
     declare_world_cmd = DeclareLaunchArgument(
         'world',
-        # TODO(orduno) Switch back once ROS argument passing has been fixed upstream
-        #              https://github.com/ROBOTIS-GIT/turtlebot3_simulations/issues/91
-        # default_value=os.path.join(get_package_share_directory('turtlebot3_gazebo'),
-        # worlds/turtlebot3_worlds/waffle.model')
         default_value=os.path.join(pkg_AirLab_in_gazebo, 'worlds', 'my_house.world'),
         description='Full path to world model file to load')
 
