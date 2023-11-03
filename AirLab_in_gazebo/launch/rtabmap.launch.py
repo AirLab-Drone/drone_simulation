c6832046940c67211b8addf08c32b6b0d4447e45
yuan-0816 <wj582693@gmail.com>
2023-11-03 20:45:06 +0800

    rtabmap launch test

--- AirLab_in_gazebo/launch/rtabmap.launch.py
+++ AirLab_in_gazebo/launch/rtabmap.launch.py
@@ -39,6 +39,12 @@ def launch_setup(context, *args, **kwargs):
     'wait_imu_to_init':True
     }]
 
+
+            # /camera/infra1/image_rect_raw,
+            # /camera/depth/image_rect_raw,
+            # /camera/infra1/camera_info
+
+
     remappings=[
           ('imu', '/imu/data'),
           ('rgb/image', '/camera/infra1/image_rect_raw'),
